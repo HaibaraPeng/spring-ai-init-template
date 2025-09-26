b307235c79834625458fe0ff8a7b8641800a4faf
Roc <rocdongai@gmail.com>
2025-09-26 14:16:08 +0800

    feat(config): 添加 MyBatis-Plus 配置类

---
+++ src/main/java/com/example/config/mybatisplus/MyBatisPlusConfiguration.java
@@ -0,0 +1,44 @@
+package com.example.config.mybatisplus;
+
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.DataChangeRecorderInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Peng
+ * @date 2025-09-26 14:14
+ */
+@Configuration
+@Slf4j
+public class MyBatisPlusConfiguration {
+
+    /**
+     * MyBatis-Plus插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        // 防止全表更新与删除插件
+        mybatisPlusInterceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+        // 数据变动记录插件
+        mybatisPlusInterceptor.addInnerInterceptor(new DataChangeRecorderInnerInterceptor());
+        // 分页插件，如果有多个插件，分页插件添加在最后，由于多数据源，所以不要指定new PaginationInnerInterceptor(DbType dbType)参数
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+
+    /**
+     * 依赖注入日志输出
+     */
+    @PostConstruct
+    private void initDi() {
+        log.info("############ {} Configuration DI.", this.getClass().getSimpleName().split("\\$\\$")[0]);
+    }
+
+}
